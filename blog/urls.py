bbd8d17ba70d23a0b49a6875f1b784c5f6f8ff7d
im-ileva <helloimileva@gmail.com>
2026-01-31 19:46:52 +0300

    rebrending in Amati-Line service

--- blog/urls.py
+++ blog/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('post/new/', views.post_new, name='post_new'),
     path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
     path('post/<pk>/remove/', views.post_remove, name='post_remove'),
+    path('catalog/', views.catalog, name='catalog'),
+
 ]
--- blog/views.py
+++ blog/views.py
@@ -5,6 +5,10 @@ from .forms import PostForm
 from django.contrib.auth.decorators import login_required
 
 
+def catalog(request):
+    return render(request, 'blog/catalog.html')
+
+
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
